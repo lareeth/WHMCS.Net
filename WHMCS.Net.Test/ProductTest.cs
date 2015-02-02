78c21014d3f7c4ba514cdda430569cf986a252d7
Gareth Luckett <gareth.luckett@gmail.com>
2015-02-02 00:42:35 +0000

    Fixed incorrect project to from .net to .Net Added Unit test project, with a single ProductName unit test.

---
+++ WHMCS.Net.Test/ProductTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WHMCS.net.Models;
+
+namespace WHMCS.Net.Test
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void ProductName()
+        {
+            Product product = new Product()
+            {
+                Name = "Product Item One"
+            };
+            Assert.AreEqual(product.Name, "Product Item One");
+        }
+    }
+}
