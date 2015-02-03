e0bdc2c55eca107be4987cfd51450b44c2db06fb
Gareth Luckett <gareth.luckett@gmail.com>
2015-02-03 14:04:05 +0000

    Removed self shunting and added Moq for mocking.

--- WHMCS.Net.Test/InvoiceTest.cs
+++ WHMCS.Net.Test/InvoiceTest.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using Moq;
+using System.Collections.Specialized;
 using System.Linq;
 using WHMCS.Net.Interfaces;
 using WHMCS.Net.Models;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace WHMCS.Net.Test
 {
-    public class InvoiceTest : IDatastore
+    public class InvoiceTest
     {
+        private string JsonData = "{\"result\":\"success\",\"invoiceid\":\"218\",\"invoicenum\":\"\",\"userid\":\"30\",\"date\":\"2015-01-29\",\"duedate\":\"2015-02-12\",\"datepaid\":\"2015-02-0218:43:58\",\"subtotal\":\"7.50\",\"credit\":\"0.00\",\"tax\":\"0.00\",\"tax2\":\"0.00\",\"total\":\"7.50\",\"balance\":\"0.00\",\"taxrate\":\"0.00\",\"taxrate2\":\"0.00\",\"status\":\"Paid\",\"paymentmethod\":\"paypal\",\"notes\":\"\",\"ccgateway\":false,\"items\":{\"item\":[{\"id\":\"311\",\"type\":\"Hosting\",\"relid\":\"37\",\"description\":\"Basic-R-example.com(12\\/02\\/2015-11\\/03\\/2015)\",\"amount\":\"7.50\",\"taxed\":\"0\"}]},\"transactions\":{\"transaction\":[{\"id\":\"162\",\"userid\":\"30\",\"currency\":\"0\",\"gateway\":\"paypal\",\"date\":\"2015-02-0218:43:58\",\"description\":\"InvoicePayment\",\"amountin\":\"7.50\",\"fees\":\"0.46\",\"amountout\":\"0.00\",\"rate\":\"1.00000\",\"transid\":\"1AB01234AB1234567\",\"invoiceid\":\"218\",\"refundid\":\"0\"}]}}";
+        private Expression<Func<IDatastore, string>> InvoiceExpectation = datastore => datastore.GetData("Website", It.IsNotNull<NameValueCollection>());
+
         [Fact]
         public void TestSingleInvoiceItemAmount()
         {
-            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", this);
+            Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
+            mockDatastore.Setup(InvoiceExpectation).Returns(JsonData);
+
+            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             InvoiceResponse singleInvoice = instance.GetInvoice(218);
             Assert.Equal("7.50", singleInvoice.Items.Item.SingleOrDefault().Amount);
         }
@@ -19,14 +28,12 @@ namespace WHMCS.Net.Test
         [Fact]
         public void TestGetSingleInvoice()
         {
-            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", this);
+            Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
+            mockDatastore.Setup(InvoiceExpectation).Returns(JsonData);
+
+            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             InvoiceResponse singleInvoice = instance.GetInvoice(218);
             Assert.NotNull(singleInvoice);
-        }
-
-        public string GetData(string url, NameValueCollection values)
-        {
-            return "{\"result\":\"success\",\"invoiceid\":\"218\",\"invoicenum\":\"\",\"userid\":\"30\",\"date\":\"2015-01-29\",\"duedate\":\"2015-02-12\",\"datepaid\":\"2015-02-0218:43:58\",\"subtotal\":\"7.50\",\"credit\":\"0.00\",\"tax\":\"0.00\",\"tax2\":\"0.00\",\"total\":\"7.50\",\"balance\":\"0.00\",\"taxrate\":\"0.00\",\"taxrate2\":\"0.00\",\"status\":\"Paid\",\"paymentmethod\":\"paypal\",\"notes\":\"\",\"ccgateway\":false,\"items\":{\"item\":[{\"id\":\"311\",\"type\":\"Hosting\",\"relid\":\"37\",\"description\":\"Basic-R-example.com(12\\/02\\/2015-11\\/03\\/2015)\",\"amount\":\"7.50\",\"taxed\":\"0\"}]},\"transactions\":{\"transaction\":[{\"id\":\"162\",\"userid\":\"30\",\"currency\":\"0\",\"gateway\":\"paypal\",\"date\":\"2015-02-0218:43:58\",\"description\":\"InvoicePayment\",\"amountin\":\"7.50\",\"fees\":\"0.46\",\"amountout\":\"0.00\",\"rate\":\"1.00000\",\"transid\":\"1AB01234AB1234567\",\"invoiceid\":\"218\",\"refundid\":\"0\"}]}}";
         }
     }
 }
--- WHMCS.Net.Test/ProductTest.cs
+++ WHMCS.Net.Test/ProductTest.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections.Specialized;
+﻿using Moq;
+using System;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Linq.Expressions;
 using WHMCS.Net.Interfaces;
 using WHMCS.Net.Models;
 using Xunit;
 
 namespace WHMCS.Net.Test
 {
-    public class ProductTest : IDatastore
+    public class ProductTest
     {
+        private string JsonData = "{\"result\":\"success\",\"totalresults\":1,\"products\":{\"product\":[{\"pid\":\"1\",\"gid\":\"1\",\"type\":\"hostingaccount\",\"name\":\"Basic cPanel Shared Hosting\",\"description\":\"    Perfect for those getting started online\r\n    2.5GB Storage\r\n    25GB Bandwidth\r\n    Free Domain *\r\n    2 Hosted Website\r\n    Support Online & Offline\r\n\",\"module\":\"cpanel\",\"paytype\":\"recurring\",\"pricing\":{\"GBP\":{\"prefix\":\"\u00a3\",\"suffix\":\"GBP\",\"msetupfee\":\"0.00\",\"qsetupfee\":\"0.00\",\"ssetupfee\":\"0.00\",\"asetupfee\":\"0.00\",\"bsetupfee\":\"0.00\",\"tsetupfee\":\"0.00\",\"monthly\":\"2.50\",\"quarterly\":\"-1.00\",\"semiannually\":\"-1.00\",\"annually\":\"25.00\",\"biennially\":\"-1.00\",\"triennially\":\"-1.00\"}},\"customfields\":{\"customfield\":[]},\"configoptions\":{\"configoption\":[]}}]}}";
+        private Expression<Func<IDatastore, string>> InvoiceExpectation = datastore => datastore.GetData("Website", It.IsNotNull<NameValueCollection>());
+
         [Fact]
         public void TestSingleProductName()
         {
-            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", this);
+            Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
+            mockDatastore.Setup(InvoiceExpectation).Returns(JsonData);
+
+            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             ProductsResponse singleProduct = instance.GetProduct(1);
             Assert.Equal("Basic cPanel Shared Hosting", singleProduct.Products.Product.SingleOrDefault().Name);
         }
@@ -19,7 +28,10 @@ namespace WHMCS.Net.Test
         [Fact]
         public void TestGetSingleProduct()
         {
-            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", this);
+            Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
+            mockDatastore.Setup(InvoiceExpectation).Returns(JsonData);
+
+            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             ProductsResponse singleProduct = instance.GetProduct(1);
             Assert.NotNull(singleProduct);
         }
@@ -27,7 +39,10 @@ namespace WHMCS.Net.Test
         [Fact]
         public void TestGetGroupProducts()
         {
-            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", this);
+            Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
+            mockDatastore.Setup(InvoiceExpectation).Returns(JsonData);
+
+            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             ProductsResponse test = instance.GetProducts(1);
             Assert.NotNull(test);
         }
@@ -35,14 +50,12 @@ namespace WHMCS.Net.Test
         [Fact]
         public void TestGetAllProducts()
         {
-            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", this);
+            Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
+            mockDatastore.Setup(InvoiceExpectation).Returns(JsonData);
+
+            WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             ProductsResponse test = instance.GetProducts();
             Assert.NotNull(test);
-        }
-
-        public string GetData(string url, NameValueCollection values)
-        {
-            return "{\"result\":\"success\",\"totalresults\":1,\"products\":{\"product\":[{\"pid\":\"1\",\"gid\":\"1\",\"type\":\"hostingaccount\",\"name\":\"Basic cPanel Shared Hosting\",\"description\":\"    Perfect for those getting started online\r\n    2.5GB Storage\r\n    25GB Bandwidth\r\n    Free Domain *\r\n    2 Hosted Website\r\n    Support Online & Offline\r\n\",\"module\":\"cpanel\",\"paytype\":\"recurring\",\"pricing\":{\"GBP\":{\"prefix\":\"\u00a3\",\"suffix\":\"GBP\",\"msetupfee\":\"0.00\",\"qsetupfee\":\"0.00\",\"ssetupfee\":\"0.00\",\"asetupfee\":\"0.00\",\"bsetupfee\":\"0.00\",\"tsetupfee\":\"0.00\",\"monthly\":\"2.50\",\"quarterly\":\"-1.00\",\"semiannually\":\"-1.00\",\"annually\":\"25.00\",\"biennially\":\"-1.00\",\"triennially\":\"-1.00\"}},\"customfields\":{\"customfield\":[]},\"configoptions\":{\"configoption\":[]}}]}}";
         }
     }
 }
