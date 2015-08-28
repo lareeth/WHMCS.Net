3bc4de96756ee91cc1db05ab78a97fa33137597f
MattTheDev <m.smithcpu@gmail.com>
2015-08-28 12:51:41 -0500

    Variable name issue - stats vs. invoice

--- WHMCS.Net.Test/StatsTest.cs
+++ WHMCS.Net.Test/StatsTest.cs
@@ -12,13 +12,13 @@ namespace WHMCS.Net.Test
     public class StatsTest
     {
         private readonly string _jsonData = "{\"result\":\"success\",\"income_today\":\"1.00\",\"income_thismonth\":\"1.00\",\"income_thisyear\":\"1.00\",\"orders_pending\":\"1\",\"orders_today_cancelled\":\"1\",\"orders_today_pending\":\"1\",\"orders_today_fraud\":\"1\",\"orders_today_active\":\"1\",\"orders_today_total\":\"1\",\"orders_yesterday_cancelled\":\"1\",\"orders_yesterday_pending\":\"123\",\"orders_yesterday_fraud\":\"1\",\"orders_yesterday_active\":\"1\",\"orders_yesterday_total\":\"1\",\"orders_thismonth_total\":\"1\",\"orders_thisyear_total\":\"1\",\"tickets_closed\":\"1\",\"tickets_inprogress\":\"1\",\"tickets_onhold\":\"1\",\"tickets_customerreply\":\"1\",\"tickets_answered\":\"1\",\"tickets_open\":\"1\",\"cancellations_pending\":\"1\",\"todoitems_due\":\"1\",\"networkissues_open\":\"1\",\"billableitems_uninvoiced\":\"1\",\"quotes_valid\":\"1\",\"staff_online\":\"1\"}";
-        private readonly Expression<Func<IDatastore, string>> _invoiceExpectation = datastore => datastore.GetData("Website", It.IsNotNull<NameValueCollection>());
+        private readonly Expression<Func<IDatastore, string>> _statsExpectation = datastore => datastore.GetData("Website", It.IsNotNull<NameValueCollection>());
 
         [Fact]
         public void TestGetStats()
         {
             Mock<IDatastore> mockDatastore = new Mock<IDatastore>();
-            mockDatastore.Setup(_invoiceExpectation).Returns(_jsonData);
+            mockDatastore.Setup(_statsExpectation).Returns(_jsonData);
 
             WhmcsApi instance = new WhmcsApi("User", "Pass", "Website", mockDatastore.Object);
             StatsResponse stats = instance.GetStats();
