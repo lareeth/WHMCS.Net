a6441c454882ac296454bd61d769e6236175c980
Gareth Luckett <gareth.luckett@gmail.com>
2015-09-01 16:46:51 +0100

    Updated assembly info to v0.4

--- WHMCS.Net/Properties/AssemblyInfo.cs
+++ WHMCS.Net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WHMCS.net")]
+[assembly: AssemblyTitle("WHMCS.Net")]
 [assembly: AssemblyDescription("A .net library allowing you to connect to a WHMCS installation and query the API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gareth Luckett")]
-[assembly: AssemblyProduct("WHMCS.net")]
+[assembly: AssemblyProduct("WHMCS.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
